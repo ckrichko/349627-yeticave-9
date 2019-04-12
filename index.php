8013845e55acf3c31e2e37bfb157f09bb06c0526
Konstantin <ckrichko@gmail.com>
2019-04-12 21:18:35 +0300

    исправляет сравнение в соответствии с критерием Д1

--- index.php
+++ index.php
@@ -27,13 +27,13 @@ $user_name = 'Константин'; // укажите здесь ваше им�
           <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
           <nav class="user-menu">
-            <?php if ($is_auth == 1) { ?>
+            <?php if ($is_auth === 1): ?>
               <div class="user-menu__logged">
-                <p><?php echo $user_name ?></p>
+                <p><?= $user_name; ?></p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
               </div>
-            <?php } else { ?>
+            <?php else: ?>
               <ul class="user-menu__list">
                 <li class="user-menu__item">
                   <a href="#">Регистрация</a>
@@ -42,7 +42,7 @@ $user_name = 'Константин'; // укажите здесь ваше им�
                   <a href="#">Вход</a>
                 </li>
               </ul>
-            <?php } ?>
+            <?php endif; ?>
           </nav>
         </div>
       </header>
