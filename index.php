c8d63678891b3f8255b92e22af89cd82b4f26349
Konstantin <ckrichko@gmail.com>
2019-04-16 12:12:09 +0300

    добавляет цикл прохода по массиву с товарами, заполняет список объявлениями

--- index.php
+++ index.php
@@ -106,25 +106,26 @@ $ads = [
             <h2>Открытые лоты</h2>
           </div>
           <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
-            <li class="lots__item lot">
-              <div class="lot__image">
-                <img src="" width="350" height="260" alt="">
-              </div>
-              <div class="lot__info">
-                <span class="lot__category">Название категории</span>
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                <div class="lot__state">
-                  <div class="lot__rate">
-                    <span class="lot__amount">Стартовая цена</span>
-                    <span class="lot__cost">цена<b class="rub">р</b></span>
-                  </div>
-                  <div class="lot__timer timer">
-                    12:23
+            <?php foreach ($ads as $key => $item) { ?>
+              <li class="lots__item lot">
+                <div class="lot__image">
+                  <img src="<?=$item['url'];?>" width="350" height="260" alt="<?=$item['title'];?>">
+                </div>
+                <div class="lot__info">
+                  <span class="lot__category"><?=$item['category'];?></span>
+                  <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['title'];?></a></h3>
+                  <div class="lot__state">
+                    <div class="lot__rate">
+                      <span class="lot__amount">Стартовая цена</span>
+                      <span class="lot__cost"><?=$item['price'];?><b class="rub">р</b></span>
+                    </div>
+                    <div class="lot__timer timer">
+                      12:23
+                    </div>
                   </div>
                 </div>
-              </div>
-            </li>
+              </li>
+            <?php } ?>
           </ul>
         </section>
       </main>
