26d8ed2e460c292f21c58ada42b5d9858e3a8bc5
Konstantin <ckrichko@gmail.com>
2019-04-16 17:30:45 +0300

    исправляет названия переменных

--- index.php
+++ index.php
@@ -94,9 +94,9 @@ $lots = [
           <p
             class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
           <ul class="promo__list">
-            <?php foreach ($categories as $item) { ?>
+            <?php foreach ($categories as $category) { ?>
               <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= $item; ?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?= $category; ?></a>
               </li>
             <?php } ?>
           </ul>
@@ -106,18 +106,18 @@ $lots = [
             <h2>Открытые лоты</h2>
           </div>
           <ul class="lots__list">
-            <?php foreach ($lots as $category) { ?>
+            <?php foreach ($lots as $lot) { ?>
               <li class="lots__item lot">
                 <div class="lot__image">
-                  <img src="<?=$category['url'];?>" width="350" height="260" alt="<?=$category['title'];?>">
+                  <img src="<?= $lot['url']; ?>" width="350" height="260" alt="<?= $lot['title']; ?>">
                 </div>
                 <div class="lot__info">
-                  <span class="lot__category"><?=$category['category'];?></span>
-                  <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$category['title'];?></a></h3>
+                  <span class="lot__category"><?= $lot['category']; ?></span>
+                  <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lot['title']; ?></a></h3>
                   <div class="lot__state">
                     <div class="lot__rate">
                       <span class="lot__amount">Стартовая цена</span>
-                      <span class="lot__cost"><?=$category['price'];?><b class="rub">р</b></span>
+                      <span class="lot__cost"><?= $lot['price']; ?><b class="rub">р</b></span>
                     </div>
                     <div class="lot__timer timer">
                       12:23
