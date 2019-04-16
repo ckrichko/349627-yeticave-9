56a89277c7e9f5fc7b77ad8bef4b74c873e9b0d2
Konstantin <ckrichko@gmail.com>
2019-04-17 00:23:25 +0300

    добавляет функцию форматирования цены товара

--- index.php
+++ index.php
@@ -43,6 +43,15 @@ $lots = [
     'url' => 'img/lot-6.jpg'
   ]
 ];
+
+function formatCurrency($number)
+{
+  ceil($number);
+  $number = number_format($number, 0, '.', ' ');
+  $number .= ' <b class="rub">р</b>';
+  return $number;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -117,7 +126,7 @@ $lots = [
                   <div class="lot__state">
                     <div class="lot__rate">
                       <span class="lot__amount">Стартовая цена</span>
-                      <span class="lot__cost"><?= $lot['price']; ?><b class="rub">р</b></span>
+                      <span class="lot__cost"><?= formatCurrency($lot['price']); ?></span>
                     </div>
                     <div class="lot__timer timer">
                       12:23
