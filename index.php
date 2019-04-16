702a60de349dc74cabfabcb2a069704b42332c2e
Konstantin <ckrichko@gmail.com>
2019-04-16 13:06:28 +0300

    исправляет названия переменных

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $user_name = 'Константин';
 
 $categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 
-$ads = [
+$lots = [
   [
     'title' => '2014 Rossignol District Snowboard',
     'category' => 'Доски и лыжи',
@@ -106,18 +106,18 @@ $ads = [
             <h2>Открытые лоты</h2>
           </div>
           <ul class="lots__list">
-            <?php foreach ($ads as $key => $item) { ?>
+            <?php foreach ($lots as $category) { ?>
               <li class="lots__item lot">
                 <div class="lot__image">
-                  <img src="<?=$item['url'];?>" width="350" height="260" alt="<?=$item['title'];?>">
+                  <img src="<?=$category['url'];?>" width="350" height="260" alt="<?=$category['title'];?>">
                 </div>
                 <div class="lot__info">
-                  <span class="lot__category"><?=$item['category'];?></span>
-                  <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['title'];?></a></h3>
+                  <span class="lot__category"><?=$category['category'];?></span>
+                  <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$category['title'];?></a></h3>
                   <div class="lot__state">
                     <div class="lot__rate">
                       <span class="lot__amount">Стартовая цена</span>
-                      <span class="lot__cost"><?=$item['price'];?><b class="rub">р</b></span>
+                      <span class="lot__cost"><?=$category['price'];?><b class="rub">р</b></span>
                     </div>
                     <div class="lot__timer timer">
                       12:23
@@ -134,9 +134,9 @@ $ads = [
     <footer class="main-footer">
       <nav class="nav">
         <ul class="nav__list container">
-          <?php foreach ($categories as $item) { ?>
+          <?php foreach ($categories as $category) { ?>
             <li class="nav__item">
-              <a href="pages/all-lots.html"><?= $item; ?></a>
+              <a href="pages/all-lots.html"><?= $category; ?></a>
             </li>
           <?php } ?>
         </ul>
