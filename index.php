67aae3bf7c40cbe1467e2cb11399a591deca6187
Konstantin <ckrichko@gmail.com>
2019-04-22 22:52:24 +0300

    использует include_template для включения всех шаблонов в index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once('functions.php');
+require_once('helpers.php');
 
 $is_auth = rand(0, 1);
 
@@ -49,4 +50,16 @@ $lots = [
     'url' => 'img/lot-6.jpg'
   ]
 ];
-?>
+
+$content = include_template('index.php',
+  ['categories' => $categories, 'lots' => $lots]);
+
+$layout_content = include_template('layout.php', [
+  'title'      => 'Главная',
+  'is_auth'    => $is_auth,
+  'user_name'  => $user_name,
+  'content'    => $content,
+  'categories' => $categories,
+]);
+
+print($layout_content);
