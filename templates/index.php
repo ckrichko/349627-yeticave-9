10714bc20721d0ea38792ac5f030345b8df3f85e
Konstantin <ckrichko@gmail.com>
2019-04-24 10:23:50 +0300

    удаляет неиспользуемую переменную

--- templates/index.php
+++ templates/index.php
@@ -15,23 +15,23 @@
     <h2>Открытые лоты</h2>
   </div>
   <ul class="lots__list">
-    <?php foreach ($lots as $lot => $key): ?>
+    <?php foreach ($lots as $lot): ?>
       <li class="lots__item lot">
         <div class="lot__image">
-          <img src="<?= htmlspecialchars($key['url']) ?>" width="350" height="260" alt="">
+          <img src="<?= htmlspecialchars($lot['url']) ?>" width="350" height="260" alt="">
         </div>
         <div class="lot__info">
-          <span class="lot__category"><?= htmlspecialchars($key['category']) ?></span>
-          <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($key['title']) ?></a>
+          <span class="lot__category"><?= htmlspecialchars($lot['category']) ?></span>
+          <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($lot['title']) ?></a>
           </h3>
           <div class="lot__state">
             <div class="lot__rate">
               <span class="lot__amount">Стартовая цена</span>
-              <span class="lot__cost"><?= formatCurrency($key['price']); ?><b class="rub">р</b></span>
+              <span class="lot__cost"><?= formatCurrency($lot['price']); ?><b class="rub">р</b></span>
             </div>
             <div
-              class="lot__timer timer <?php if (timeToEndLessOneHour($key['time'])): ?>timer--finishing<?php endif; ?>">
-              <?= timeToEnd($key['time']) ?>
+              class="lot__timer timer <?php if (timeToEndLessOneHour($lot['time'])): ?>timer--finishing<?php endif; ?>">
+              <?= timeToEnd($lot['time']) ?>
             </div>
           </div>
         </div>
