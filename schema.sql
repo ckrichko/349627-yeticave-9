54fe87f4bcd5034147ab5e9d1751e25a1aae38b2
Konstantin <ckrichko@gmail.com>
2019-04-25 17:35:46 +0300

    создаёт схему БД личного проекта

--- schema.sql
+++ schema.sql
@@ -1,23 +1,25 @@
-CREATE DATABASE yeticave character set UTF8 collate utf8_general_ci;
+CREATE DATABASE yeticave
+  CHARACTER SET UTF8
+  COLLATE utf8_general_ci;
 USE yeticave;
 
 CREATE TABLE category
 (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(50) NOT NULL,
-  symbol_code varchar(50) NOT NULL,
+  id          INT         NOT NULL AUTO_INCREMENT,
+  name        VARCHAR(50) NOT NULL,
+  symbol_code VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE user
 (
-  id int NOT NULL AUTO_INCREMENT,
-  email varchar(50) NOT NULL,
-  password varchar(50) NOT NULL,
-  name varchar(50) NOT NULL,
-  contact varchar(255) NOT NULL,
-  avatar varchar(50),
-  reg_date datetime DEFAULT NOW(),
+  id       INT          NOT NULL AUTO_INCREMENT,
+  email    VARCHAR(50)  NOT NULL,
+  password VARCHAR(50)  NOT NULL,
+  name     VARCHAR(50)  NOT NULL,
+  contact  VARCHAR(255) NOT NULL,
+  avatar   VARCHAR(50),
+  reg_date DATETIME              DEFAULT NOW(),
   PRIMARY KEY (id),
   INDEX (password),
   UNIQUE INDEX (name, email)
@@ -25,32 +27,32 @@ CREATE TABLE user
 
 CREATE TABLE lot
 (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(50) NOT NULL,
-  description varchar(200) NOT NULL,
-  image varchar(50) NOT NULL,
-  id_category int NOT NULL,
-  start_price int NOT NULL,
-  end_date datetime NOT NULL,
-  bid_step int NOT NULL,
-  creation_date datetime DEFAULT NOW(),
-  id_creator int NOT NULL,
-  id_winner int,
+  id            INT          NOT NULL AUTO_INCREMENT,
+  name          VARCHAR(50)  NOT NULL,
+  description   VARCHAR(200) NOT NULL,
+  image         VARCHAR(50)  NOT NULL,
+  id_category   INT          NOT NULL,
+  start_price   INT          NOT NULL,
+  end_date      DATETIME     NOT NULL,
+  bid_step      INT          NOT NULL,
+  creation_date DATETIME              DEFAULT NOW(),
+  id_creator    INT          NOT NULL,
+  id_winner     INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (id_creator) REFERENCES user(id),
-  FOREIGN KEY (id_category) REFERENCES category(id),
+  FOREIGN KEY (id_creator) REFERENCES user (id),
+  FOREIGN KEY (id_category) REFERENCES category (id),
   INDEX (end_date, id_creator, id_winner)
 );
 
 CREATE TABLE bid
 (
-  id int NOT NULL AUTO_INCREMENT,
-  amount int NOT NULL,
-  creation_date datetime DEFAULT NOW(),
-  id_user int NOT NULL,
-  id_lot int NOT NULL,
+  id            INT NOT NULL AUTO_INCREMENT,
+  amount        INT NOT NULL,
+  creation_date DATETIME     DEFAULT NOW(),
+  id_user       INT NOT NULL,
+  id_lot        INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (id_user) REFERENCES user(id),
-  FOREIGN KEY (id_lot) REFERENCES lot(id),
+  FOREIGN KEY (id_user) REFERENCES user (id),
+  FOREIGN KEY (id_lot) REFERENCES lot (id),
   INDEX (id_user, id_lot)
 )
