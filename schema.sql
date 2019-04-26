a5bc85bef23233e7b3f0415dc73a308745fc2098
Konstantin <ckrichko@gmail.com>
2019-04-26 15:48:10 +0300

    исправляет названия полей

--- schema.sql
+++ schema.sql
@@ -19,39 +19,39 @@ CREATE TABLE users
   name       VARCHAR(64) NOT NULL,
   contact    TEXT(255)   NOT NULL,
   avatar_url VARCHAR(255),
-  created_at   DATETIME             DEFAULT NOW(),
+  created_at DATETIME             DEFAULT NOW(),
   PRIMARY KEY (id),
   UNIQUE INDEX (email)
 );
 
 CREATE TABLE lot
 (
-  id            INT          NOT NULL AUTO_INCREMENT,
-  name          VARCHAR(255) NOT NULL,
-  description   TEXT(1024)   NOT NULL,
-  image_url     VARCHAR(255) NOT NULL,
-  id_category   INT          NOT NULL,
-  start_price   INT          NOT NULL,
-  end_date      DATETIME     NOT NULL,
-  bid_step      INT          NOT NULL,
-  creation_date DATETIME              DEFAULT NOW(),
-  id_creator    INT          NOT NULL,
-  id_winner     INT,
+  id          INT          NOT NULL AUTO_INCREMENT,
+  name        VARCHAR(255) NOT NULL,
+  description TEXT(1024)   NOT NULL,
+  image_url   VARCHAR(255) NOT NULL,
+  category_id INT          NOT NULL,
+  start_price INT          NOT NULL,
+  end_at      DATETIME     NOT NULL,
+  bid_step    INT          NOT NULL,
+  created_at  DATETIME              DEFAULT NOW(),
+  creator_id  INT          NOT NULL,
+  winner_id   INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (id_creator) REFERENCES users (id),
-  FOREIGN KEY (id_category) REFERENCES categories (id),
-  INDEX (end_date, id_creator, id_winner)
+  FOREIGN KEY (creator_id) REFERENCES users (id),
+  FOREIGN KEY (category_id) REFERENCES categories (id),
+  INDEX (end_at, creator_id, winner_id)
 );
 
 CREATE TABLE bid
 (
-  id            INT NOT NULL AUTO_INCREMENT,
-  amount        INT NOT NULL,
-  creation_date DATETIME     DEFAULT NOW(),
-  id_user       INT NOT NULL,
-  id_lot        INT NOT NULL,
+  id         INT NOT NULL AUTO_INCREMENT,
+  amount     INT NOT NULL,
+  created_at DATETIME     DEFAULT NOW(),
+  user_id    INT NOT NULL,
+  lot_id     INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (id_user) REFERENCES users (id),
-  FOREIGN KEY (id_lot) REFERENCES lot (id),
-  INDEX (id_user, id_lot)
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (lot_id) REFERENCES lot (id),
+  INDEX (user_id, lot_id)
 )
