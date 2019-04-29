873ebc45ff8020d7eca74c7a9f61fdfb373e3bba
Konstantin <ckrichko@gmail.com>
2019-04-29 16:55:21 +0300

    исправляет ошибки в названии полей

--- queries.sql
+++ queries.sql
@@ -5,7 +5,7 @@ INSERT INTO users
 (id, email, password, name, contact, avatar_url) VALUES ('1', 'ckrichko@gmail.com', 'A.cpjE^5HW/1o', 'Константин', 'ул. Вишнёвая, 20', 'img/avatar.jpg'), ('2', 'vasya_pupkin@mail.ru', '=145.da37-a44', 'Василий', 'пр-т. Рабиновича, 1, к.2', 'img/avatar.jpg');
 
 INSERT INTO lots
-(id, name, description, img_url, category_id, start_price, end_at, bid_step, creator_id) VALUES
+(id, name, description, image_url, category_id, start_price, end_at, bid_step, creator_id) VALUES
 	('1', '2014 Rossignol District Snowboard', 'Описание лота №1', 'img/lot-1.jpg', '1', '10999', '2019-04-29', '100', '1'),
 	('2', 'DC Ply Mens 2016/2017 Snowboard', 'Описание лота №2', 'img/lot-2.jpg', '1', '159999', '2019-04-30', '100', '2'),
 	('3', 'Крепления Union Contact Pro 2015 года размер L/XL', 'Описание лота №3', 'img/lot-3.jpg', '2', '8000', '2019-04-28', '100', '1'),
